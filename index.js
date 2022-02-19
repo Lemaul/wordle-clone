73f58c0b63b05be3971733be10145e9028f4d895
Lemaul <pawel.tuczynski1@gmail.com>
2022-02-19 17:12:30 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,160 @@
+'use strict'
+
+const wordList = [
+    'plane',
+    'plate',
+    'kitty',
+    'crane',
+    'shift',
+    'apple',
+    'tired',
+    'talks',
+    'tired',
+    'kites',
+    'point',
+    'fails',
+    'tiles',
+    'stale',
+    'mines',
+]
+
+const winningWord = wordList[Math.floor(Math.random()*wordList.length)];
+console.log(winningWord);
+
+let grid = document.getElementById('grid');
+let currentAttemptNumber = 0;
+let currentColumnNumber = 0;
+
+function drawGrid() {
+    for (let i = 0; i < 6; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < 5; j++) {
+            let tile = document.createElement('div');
+            tile.classList.add('tile');
+            row.append(tile);
+        }
+        grid.append(row);
+    }
+}
+
+drawGrid();
+
+let pressedLetter = (e, tile) => {
+    if (tile.nextSibling) {
+        if (tile.textContent) {
+            tile = tile.nextSibling;
+            currentColumnNumber++;
+        }
+        tile.textContent = e.key;
+    }
+    else {
+        tile.textContent = tile.textContent || e.key;
+    }
+    tile.setAttribute("data-letter", "notConfirmed");
+}
+
+let pressedBackspace = (tile) => {
+    if (tile.previousSibling) {
+        if (!tile.textContent) {
+            tile = tile.previousSibling;
+            currentColumnNumber--;
+        }
+        tile.textContent = "";
+    }
+    else {
+        tile.textContent = "";
+    }
+    tile.removeAttribute("data-letter")
+}
+
+const win = () => {
+    alert(`You won on attempt number ${currentAttemptNumber+1}\nRefresh to play again`);
+    pressedLetter = () => {};
+    pressedBackspace = () => {};
+    pressedEnter = () => {};
+}
+
+const lose = () => {
+    alert('You lost!!\nRefresh to play again');
+    pressedLetter = () => {};
+    pressedBackspace = () => {};
+    pressedEnter = () => {};
+}
+
+const notEnoughLetters = () => {
+    alert('not enough letters!')
+}
+
+const notInWordList = () => {
+    alert('not in word list!')
+}
+
+const colorCurrentAttempt = (row) => {
+    Array.from(row.children).forEach((tile, i) => {
+        const tileText = tile.textContent;
+        const indexes = [...winningWord].reduce((indexes, char, idx) => {
+           return char === tileText ? [...indexes, idx] : indexes
+        }, []);
+        if (indexes.length === 0) {
+            tile.setAttribute("data-letter", "absent");
+        }
+        else if (indexes.includes(i)) {
+            tile.setAttribute("data-letter", "correct");
+        }
+        else {
+            tile.setAttribute("data-letter", "present");
+        }
+    })
+}
+
+let pressedEnter = (row) => {
+    let word = "";
+    Array.from(row.children).forEach(
+        tile => word += tile.textContent
+    );
+
+    if (word.length < 5) {
+        notEnoughLetters();
+        return;
+    }
+    if (wordList.indexOf(word) < 0) {
+        notInWordList();
+        return;
+    }
+
+    colorCurrentAttempt(row);
+
+    if (row.nextSibling) {
+        if (word !== winningWord) {
+            currentAttemptNumber++;
+            currentColumnNumber = 0;
+        }
+        else {
+            win();
+        }
+    }
+    // todo export into a function once there will be animation and stuff
+    else {
+        if (word === winningWord) {
+            win();
+            return;
+        }
+        lose();
+    }
+}
+
+
+function updateGrid(e) {
+    let row = grid.children[currentAttemptNumber];
+    let tile = row.children[currentColumnNumber];
+    const letterCode = e.key.charCodeAt(0);
+    if (letterCode >= 97 && letterCode <= 122)
+        pressedLetter(e, tile);
+    else if (e.key === "Backspace")
+        pressedBackspace(tile);
+    else if (e.key === "Enter")
+        pressedEnter(row);
+}
+
+document.addEventListener('keydown', (e) => updateGrid(e))
