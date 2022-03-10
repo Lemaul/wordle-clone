a6921c51bde907090afb459f637b24654fbaf0a6
Lemaul <pawel.tuczynski1@gmail.com>
2022-03-10 21:52:32 +0100

    added keyboard below the grid

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,11 @@
 'use strict';
 import { wordList } from './wordList.js';
 import { drawGrid } from './grid.js';
+import { drawKeyboard } from './keyboard.js';
 
 export const winningWord =
     wordList[Math.floor(Math.random() * wordList.length)];
 // console.log(winningWord);
 
 drawGrid();
+drawKeyboard();
---
+++ src/scripts/keyboard.js
@@ -0,0 +1,54 @@
+import { keysOrder } from './keysOrder.js';
+import { winningWord } from './index.js';
+
+let keyboard = document.getElementById('keyboard');
+
+let keyboardState = {};
+
+export const drawKeyboard = () => {
+    keysOrder.forEach(row => {
+        let keyboardRow = document.createElement('div');
+        keyboardRow.classList.add('row');
+        row.forEach(key => {
+            let keyboardKey = document.createElement('div');
+            keyboardKey.classList.add('letter-key');
+            keyboardKey.id = `key-${key}`;
+            keyboardKey.textContent = key;
+            if (!key) {
+                keyboardKey.classList.add('half-key');
+            }
+            if (key === 'del' || key === 'enter')
+                keyboardKey.classList.add('functional-key');
+            keyboardRow.appendChild(keyboardKey);
+        });
+        keyboard.append(keyboardRow);
+    });
+};
+
+export const updateKeyboard = row => {
+    let winningLetters = Array.from(winningWord);
+    let currentLetters = Array.from(row.children).map(tile => tile.textContent);
+    currentLetters.forEach((letter, i) => {
+        if (!keyboardState[letter]) {
+            keyboardState[letter] = 'absent';
+        }
+        if (
+            keyboardState[letter] === 'absent' &&
+            winningLetters.includes(letter)
+        ) {
+            keyboardState[letter] = 'present';
+        }
+        if (
+            keyboardState[letter] === 'present' &&
+            letter === winningLetters[i]
+        ) {
+            keyboardState[letter] = 'correct';
+        }
+    });
+    Object.entries(keyboardState).forEach(entry => {
+        const [key, dataLetter] = entry;
+        document
+            .getElementById(`key-${key}`)
+            .setAttribute('data-letter', dataLetter);
+    });
+};
